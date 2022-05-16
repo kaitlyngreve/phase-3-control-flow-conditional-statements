9aa9815e7517e3388210c302da255012fe337652
Kaitlyn Greve <kgreve14@gmail.com>
2022-05-16 13:35:09 -0400

    commit tree 71500c825cc1e11331f8b9aad0a8457126e0f7ca

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,19 @@
-def admin_login(username, password)
+def admin_login username, password
+  if username == "admin" || username == "ADMIN" && password == 12345
+    "Access granted"
+  elsif "Access denied"
+  end
+end
+
+def hows_the_weather temperature
   # your code here
 end
 
-def hows_the_weather(temperature)
+def fizzbuzz num
   # your code here
 end
 
-def fizzbuzz(num)
-  # your code here
-end
-
-def calculator(operation, num1, num2)
+def calculator operation, num1, num2
   # your code here
 end
 
